ad2d45ad6db5f128bf7b608519e3260ff161ce2b
mlhiter <3076438032@qq.com>
2025-03-01 20:29:49 +0800

    style: create/network

--- frontend/providers/devbox/app/[lang]/(platform)/devbox/create/components/form/Network.tsx
+++ frontend/providers/devbox/app/[lang]/(platform)/devbox/create/components/form/Network.tsx
@@ -14,12 +14,12 @@ import {
   Switch,
   useTheme
 } from '@chakra-ui/react';
-import { MySelect, useMessage } from '@sealos/ui';
-import { useTranslations } from 'next-intl';
-import dynamic from 'next/dynamic';
 import { useState } from 'react';
+import dynamic from 'next/dynamic';
+import { useTranslations } from 'next-intl';
+import { MySelect, useMessage } from '@sealos/ui';
 import { useFieldArray, useFormContext } from 'react-hook-form';
-// const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz', 12)
+import Label from './Label';
 
 export type CustomAccessModalParams = {
   publicDomain: string;
@@ -31,8 +31,11 @@ const AppendNetworksButton = (props: ButtonProps) => {
   const t = useTranslations();
   return (
     <Button
-      w={'100px'}
+      boxShadow={'none'}
+      w={'110px'}
       variant={'outline'}
+      fontWeight={'normal'}
+      color={'grayModern.900'}
       leftIcon={<MyIcon name="plus" w={'18px'} fill={'#485264'} />}
       {...props}
     >
@@ -71,159 +74,177 @@ export default function NetworkConfiguration({ isEdit, ...props }: BoxProps & { 
   return (
     <>
       <Box id={'baseInfo'} {...props}>
-        <Box px={'42px'} py={'24px'} userSelect={'none'}>
+        <Box h={'30px'}>
+          <Label w={100}>{t('network_settings')}</Label>
+        </Box>
+        <Box py={'8px'} userSelect={'none'}>
           {networks.length === 0 && <AppendNetworksButton onClick={() => appendNetworks()} />}
           {networks.map((network, i) => (
-            <Flex
-              className="guide-network-configuration"
-              alignItems={'flex-start'}
-              key={network.id}
-              _notLast={{ pb: 6, borderBottom: theme.borders.base }}
-              _notFirst={{ pt: 6 }}
-            >
-              <Box>
-                <Box mb={'10px'} h={'20px'} fontSize={'base'} color={'grayModern.900'}>
-                  {t('Container Port')}
-                </Box>
-                <Input
-                  h={'32px'}
-                  type={'number'}
-                  w={'110px'}
-                  bg={'grayModern.50'}
-                  {...register(`networks.${i}.port`, {
-                    valueAsNumber: true,
-                    min: {
-                      value: 1,
-                      message: t('The minimum exposed port is 1')
-                    },
-                    max: {
-                      value: 65535,
-                      message: t('The maximum number of exposed ports is 65535')
-                    },
-                    validate: {
-                      repeatPort: (value) => {
-                        const ports = getValues('networks').map((network, index) => ({
-                          port: network.port,
-                          index
-                        }));
-                        // 排除当前正在编辑的端口
-                        const isDuplicate = ports.some(
-                          (item) => item.port === value && item.index !== i
-                        );
-                        return !isDuplicate || t('The port number cannot be repeated');
+            <>
+              <Flex
+                className="guide-network-configuration"
+                alignItems={'flex-start'}
+                key={network.id}
+                _notLast={{ pb: 6, borderBottom: theme.borders.base }}
+                _notFirst={{ pt: 6 }}
+              >
+                <Box>
+                  <Box
+                    mb={'10px'}
+                    h={'20px'}
+                    fontSize={'base'}
+                    color={'grayModern.900'}
+                    fontWeight={'bold'}
+                  >
+                    {t('Container Port')}
+                  </Box>
+                  <Input
+                    h={'35px'}
+                    type={'number'}
+                    w={'110px'}
+                    bg={'white'}
+                    {...register(`networks.${i}.port`, {
+                      valueAsNumber: true,
+                      min: {
+                        value: 1,
+                        message: t('The minimum exposed port is 1')
+                      },
+                      max: {
+                        value: 65535,
+                        message: t('The maximum number of exposed ports is 65535')
+                      },
+                      validate: {
+                        repeatPort: (value) => {
+                          const ports = getValues('networks').map((network, index) => ({
+                            port: network.port,
+                            index
+                          }));
+                          // 排除当前正在编辑的端口
+                          const isDuplicate = ports.some(
+                            (item) => item.port === value && item.index !== i
+                          );
+                          return !isDuplicate || t('The port number cannot be repeated');
+                        }
                       }
-                    }
-                  })}
-                />
-                {i === networks.length - 1 && networks.length < 5 && (
-                  <Box mt={3}>
-                    <AppendNetworksButton onClick={() => appendNetworks()} />
+                    })}
+                  />
+                </Box>
+                <Box mx={7}>
+                  <Box
+                    mb={'8px'}
+                    h={'20px'}
+                    fontSize={'base'}
+                    color={'grayModern.900'}
+                    fontWeight={'bold'}
+                  >
+                    {t('Open Public Access')}
+                  </Box>
+                  <Flex alignItems={'center'} h={'35px'}>
+                    <Switch
+                      className="driver-deploy-network-switch"
+                      size={'lg'}
+                      id={`openPublicDomain-${i}`}
+                      isChecked={!!network.openPublicDomain}
+                      onChange={(e) => {
+                        const devboxName = getValues('name');
+                        if (!devboxName) {
+                          toast({
+                            title: t('Please enter the devbox name first'),
+                            status: 'warning'
+                          });
+                          return;
+                        }
+                        updateNetworks(i, {
+                          ...getValues('networks')[i],
+                          networkName: network.networkName || `${devboxName}-${nanoid()}`,
+                          protocol: network.protocol || 'HTTP',
+                          openPublicDomain: e.target.checked,
+                          publicDomain: network.publicDomain || `${nanoid()}.${env.ingressDomain}`
+                        });
+                      }}
+                    />
+                  </Flex>
+                </Box>
+                {network.openPublicDomain && (
+                  <>
+                    <Box flex={'1 0 0'}>
+                      <Box mb={'8px'} h={'20px'}></Box>
+                      <Flex alignItems={'center'} h={'35px'}>
+                        <MySelect
+                          width={'100px'}
+                          height={'35px'}
+                          bg={'white'}
+                          borderTopRightRadius={0}
+                          borderBottomRightRadius={0}
+                          value={network.protocol}
+                          // border={theme.borders.base}
+                          list={ProtocolList}
+                          onchange={(val: any) => {
+                            updateNetworks(i, {
+                              ...getValues('networks')[i],
+                              protocol: val
+                            });
+                          }}
+                        />
+                        <Flex
+                          bg={'white'}
+                          maxW={'350px'}
+                          flex={'1 0 0'}
+                          alignItems={'center'}
+                          h={'35px'}
+                          px={2}
+                          border={theme.borders.base}
+                          borderLeft={0}
+                          borderTopRightRadius={'md'}
+                          borderBottomRightRadius={'md'}
+                        >
+                          <Box flex={1} userSelect={'all'} className="textEllipsis">
+                            {network.customDomain ? network.customDomain : network.publicDomain!}
+                          </Box>
+                          <Box
+                            fontSize={'11px'}
+                            color={'#224EF5'}
+                            cursor={'pointer'}
+                            whiteSpace={'nowrap'}
+                            onClick={() =>
+                              setCustomAccessModalData({
+                                publicDomain: network.publicDomain!,
+                                customDomain: network.customDomain!
+                              })
+                            }
+                          >
+                            {t('Custom Domain')}
+                          </Box>
+                        </Flex>
+                      </Flex>
+                    </Box>
+                  </>
+                )}
+                {networks.length >= 1 && (
+                  <Box ml={3}>
+                    <Box mb={'8px'} h={'20px'}></Box>
+                    <IconButton
+                      height={'35px'}
+                      width={'35px'}
+                      aria-label={'button'}
+                      variant={'outline'}
+                      bg={'#FFF'}
+                      _hover={{
+                        color: 'red.600',
+                        bg: 'rgba(17, 24, 36, 0.05)'
+                      }}
+                      icon={<MyIcon name={'delete'} w={'16px'} fill={'#485264'} />}
+                      onClick={() => removeNetworks(i)}
+                    />
                   </Box>
                 )}
-              </Box>
-              <Box mx={7}>
-                <Box mb={'8px'} h={'20px'} fontSize={'base'} color={'grayModern.900'}>
-                  {t('Open Public Access')}
-                </Box>
-                <Flex alignItems={'center'} h={'35px'}>
-                  <Switch
-                    className="driver-deploy-network-switch"
-                    size={'lg'}
-                    id={`openPublicDomain-${i}`}
-                    isChecked={!!network.openPublicDomain}
-                    onChange={(e) => {
-                      const devboxName = getValues('name');
-                      if (!devboxName) {
-                        toast({
-                          title: t('Please enter the devbox name first'),
-                          status: 'warning'
-                        });
-                        return;
-                      }
-                      updateNetworks(i, {
-                        ...getValues('networks')[i],
-                        networkName: network.networkName || `${devboxName}-${nanoid()}`,
-                        protocol: network.protocol || 'HTTP',
-                        openPublicDomain: e.target.checked,
-                        publicDomain: network.publicDomain || `${nanoid()}.${env.ingressDomain}`
-                      });
-                    }}
-                  />
-                </Flex>
-              </Box>
-              {network.openPublicDomain && (
-                <>
-                  <Box flex={'1 0 0'}>
-                    <Box mb={'8px'} h={'20px'}></Box>
-                    <Flex alignItems={'center'} h={'35px'}>
-                      <MySelect
-                        width={'100px'}
-                        height={'32px'}
-                        borderTopRightRadius={0}
-                        borderBottomRightRadius={0}
-                        value={network.protocol}
-                        // border={theme.borders.base}
-                        list={ProtocolList}
-                        onchange={(val: any) => {
-                          updateNetworks(i, {
-                            ...getValues('networks')[i],
-                            protocol: val
-                          });
-                        }}
-                      />
-                      <Flex
-                        maxW={'350px'}
-                        flex={'1 0 0'}
-                        alignItems={'center'}
-                        h={'32px'}
-                        bg={'grayModern.50'}
-                        px={2}
-                        border={theme.borders.base}
-                        borderLeft={0}
-                        borderTopRightRadius={'md'}
-                        borderBottomRightRadius={'md'}
-                      >
-                        <Box flex={1} userSelect={'all'} className="textEllipsis">
-                          {network.customDomain ? network.customDomain : network.publicDomain!}
-                        </Box>
-                        <Box
-                          fontSize={'11px'}
-                          color={'brightBlue.600'}
-                          cursor={'pointer'}
-                          whiteSpace={'nowrap'}
-                          onClick={() =>
-                            setCustomAccessModalData({
-                              publicDomain: network.publicDomain!,
-                              customDomain: network.customDomain!
-                            })
-                          }
-                        >
-                          {t('Custom Domain')}
-                        </Box>
-                      </Flex>
-                    </Flex>
-                  </Box>
-                </>
-              )}
-              {networks.length >= 1 && (
-                <Box ml={3}>
-                  <Box mb={'8px'} h={'20px'}></Box>
-                  <IconButton
-                    height={'32px'}
-                    width={'32px'}
-                    aria-label={'button'}
-                    variant={'outline'}
-                    bg={'#FFF'}
-                    _hover={{
-                      color: 'red.600',
-                      bg: 'rgba(17, 24, 36, 0.05)'
-                    }}
-                    icon={<MyIcon name={'delete'} w={'16px'} fill={'#485264'} />}
-                    onClick={() => removeNetworks(i)}
-                  />
+              </Flex>
+              {i === networks.length - 1 && networks.length < 5 && (
+                <Box mt={3}>
+                  <AppendNetworksButton onClick={() => appendNetworks()} />
                 </Box>
               )}
-            </Flex>
+            </>
           ))}
         </Box>
       </Box>
