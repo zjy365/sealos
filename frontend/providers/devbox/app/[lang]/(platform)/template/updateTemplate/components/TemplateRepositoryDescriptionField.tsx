5a35faebc32ae4fa7c73dc6b2659564fabe17aa0
mlhiter <3076438032@qq.com>
2025-03-03 16:24:54 +0800

    style: detail/template

--- frontend/providers/devbox/app/[lang]/(platform)/template/updateTemplate/components/TemplateRepositoryDescriptionField.tsx
+++ frontend/providers/devbox/app/[lang]/(platform)/template/updateTemplate/components/TemplateRepositoryDescriptionField.tsx
@@ -1,12 +1,14 @@
-import MyFormLabel from '@/components/MyFormControl';
-import { Flex, Textarea } from '@chakra-ui/react';
 import { useTranslations } from 'next-intl';
+import { Flex, Textarea } from '@chakra-ui/react';
 import { Controller, useFormContext } from 'react-hook-form';
+
+import MyFormLabel from '@/components/MyFormControl';
+
 export default function TemplateRepositoryDescriptionField() {
   const { control } = useFormContext<{ description: string }>();
   const t = useTranslations();
   return (
-    <Flex align={'center'}>
+    <Flex align={'start'} direction={'column'} gap={4} w={'full'}>
       <MyFormLabel width="108px" m="0" fontSize="14px">
         {t('template_description')}
       </MyFormLabel>
@@ -17,13 +19,12 @@ export default function TemplateRepositoryDescriptionField() {
           <Textarea
             {...field}
             placeholder={t('template_description_placeholder')}
-            bg="grayModern.50"
+            bg="white"
             borderColor="grayModern.200"
             color={'grayModern.500'}
             fontSize={'12px'}
             resize="vertical"
             minH="106px"
-            width={'350px'}
           />
         )}
       />
