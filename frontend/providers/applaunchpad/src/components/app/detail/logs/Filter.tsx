4038b26f0a99067c5f6b87a97549ce6260719957
zjy <3161362058@qq.com>
2025-03-11 16:05:44 +0800

    update log

--- frontend/providers/applaunchpad/src/components/app/detail/logs/Filter.tsx
+++ frontend/providers/applaunchpad/src/components/app/detail/logs/Filter.tsx
@@ -13,7 +13,10 @@ import {
   Portal,
   BoxProps,
   InputGroup,
-  InputLeftElement
+  InputLeftElement,
+  PopoverTrigger,
+  PopoverContent,
+  Popover
 } from '@chakra-ui/react';
 import { MySelect } from '@sealos/ui';
 import { debounce } from 'lodash';
@@ -65,108 +68,121 @@ export const Filter = ({
   }, [isOpen, onClose]);
 
   return (
-    <Box position="relative">
-      <Button
-        ref={btnRef}
-        onClick={() => {
-          onToggle();
-          formHook.setValue('isJsonMode', !isJsonMode);
-          formHook.setValue('jsonFilters', []);
-        }}
-        variant="outline"
-        width="fit-content"
-        h={'40px'}
-        boxShadow={'none'}
-        bg={'#fff'}
-        color={'#181818'}
-        borderRadius={'8px'}
-        border={'1px dashed #D4D4D4'}
-        leftIcon={<FilterIcon size={16} color="#737373" />}
-      >
-        {t('by_json')}
-      </Button>
-
-      <Portal>
-        {isOpen && (
-          <FloatingBox
-            ref={menuRef}
-            position="fixed"
-            top={btnRef.current ? btnRef.current.getBoundingClientRect().bottom + 4 : '44px'}
-            left={btnRef.current ? btnRef.current.getBoundingClientRect().left : 0}
-            zIndex={1000}
-            boxShadow="lg"
-            bg="white"
-            borderRadius="md"
-            maxW="90vw"
-            w="auto"
-            onClick={(e) => e.stopPropagation()}
-          >
+    <Popover placement={'bottom-start'}>
+      <PopoverTrigger>
+        <Button
+          ref={btnRef}
+          onClick={() => {
+            onToggle();
+            formHook.setValue('isJsonMode', !isJsonMode);
+            formHook.setValue('jsonFilters', []);
+          }}
+          variant="outline"
+          width="fit-content"
+          h={'40px'}
+          boxShadow={'none'}
+          bg={'#fff'}
+          color={'#181818'}
+          borderRadius={'8px'}
+          border={'1px dashed #D4D4D4'}
+          leftIcon={<FilterIcon size={16} color="#737373" />}
+        >
+          {t('by_json')}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent left={0} border={'1px solid #E4E4E7'}>
+        <Flex p={'6'} flexDir={'column'} borderRadius="md" bg="white">
+          {isJsonMode && (
             <Flex
-              p={'6'}
-              flexDir={'column'}
-              border="1px solid"
-              borderColor="gray.200"
-              borderRadius="md"
-              bg="white"
+              w={'100%'}
+              bg={'grayModern.50'}
+              minH={'40px'}
+              p={'12px'}
+              gap={'12px'}
+              flexWrap={'wrap'}
+              borderRadius={'0px 8px 8px 8px'}
             >
-              <Flex gap={'18px'} flexWrap="wrap">
-                <Flex alignItems={'center'} gap={'12px'}>
-                  <Text
-                    fontSize={'12px'}
-                    fontWeight={'500'}
-                    lineHeight={'16px'}
-                    color={'grayModern.900'}
-                  >
-                    {t('only_stderr')}
+              {filterKeys.length > 0 || fields.length > 0 ? (
+                <AppendJSONFormItemButton
+                  onClick={() =>
+                    append({
+                      key: '',
+                      value: '',
+                      mode: '='
+                    })
+                  }
+                />
+              ) : (
+                <Center flex={1}>
+                  <Text fontSize={'12px'} fontWeight={'400'} lineHeight={'16px'} py={'4px'}>
+                    {t('no_data_available')}
                   </Text>
-                  <Switch
-                    isChecked={isOnlyStderr}
-                    onChange={() => formHook.setValue('isOnlyStderr', !isOnlyStderr)}
+                </Center>
+              )}
+
+              {fields.map((field, index) => (
+                <Flex key={field.id} w={'fit-content'} gap={'2px'}>
+                  <MySelect
+                    height="32px"
+                    minW={'160px'}
+                    bg={'white'}
+                    color={'grayModern.600'}
+                    placeholder={t('field_name')}
+                    value={formHook.watch(`jsonFilters.${index}.key`)}
+                    list={formHook.watch('filterKeys')}
+                    onchange={(val: string) => formHook.setValue(`jsonFilters.${index}.key`, val)}
                   />
-                </Flex>
-                <InputGroup w={'200px'}>
-                  <InputLeftElement pointerEvents="none" h={'40px'} w={'40px'}>
-                    <Search color="#666666" size={16} />
-                  </InputLeftElement>
-                  <Input
-                    height="40px"
-                    w={'300px'}
-                    width={'fit-content'}
-                    bg={'#F5F5F5'}
-                    border={'none'}
-                    placeholder={t('search_keyword')}
-                    _hover={{
-                      bg: 'white',
-                      border: '1px solid #E8EBF0'
-                    }}
-                    _focus={{
-                      border: '1px solid #E8EBF0'
-                    }}
-                    value={inputKeyword}
-                    onChange={(e) =>
-                      debounce(() => {
-                        setInputKeyword(e.target.value);
-                        formHook.setValue('keyword', e.target.value);
-                        refetchData();
-                      }, 3000)
+                  <MySelect
+                    height="32px"
+                    minW={'60px'}
+                    bg={'white'}
+                    color={'grayModern.600'}
+                    value={formHook.watch(`jsonFilters.${index}.mode`)}
+                    list={
+                      [
+                        { value: '=', label: t('equal') },
+                        { value: '!=', label: t('not_equal') },
+                        { value: '~', label: t('contains') },
+                        { value: '!~', label: t('not_contains') }
+                      ] as { value: JsonFilterItem['mode']; label: string }[]
+                    }
+                    onchange={(val: string) =>
+                      formHook.setValue(`jsonFilters.${index}.mode`, val as JsonFilterItem['mode'])
                     }
                   />
-                </InputGroup>
-              </Flex>
-
-              {/* json mode */}
-              {isJsonMode && (
-                <Flex
-                  w={'100%'}
-                  bg={'grayModern.50'}
-                  minH={'40px'}
-                  mt={'24px'}
-                  p={'12px'}
-                  gap={'12px'}
-                  flexWrap={'wrap'}
-                  borderRadius={'0px 8px 8px 8px'}
-                >
-                  {filterKeys.length > 0 || fields.length > 0 ? (
+                  <Input
+                    maxW={'160px'}
+                    placeholder={t('value')}
+                    bg={'white'}
+                    value={formHook.watch(`jsonFilters.${index}.value`)}
+                    onChange={(e) =>
+                      formHook.setValue(`jsonFilters.${index}.value`, e.target.value)
+                    }
+                    border={'1px solid #E8EBF0'}
+                    boxShadow={
+                      '0px 1px 2px 0px rgba(19, 51, 107, 0.05),0px 0px 1px 0px rgba(19, 51, 107, 0.08)'
+                    }
+                  />
+                  <Button
+                    variant={'outline'}
+                    h={'32px'}
+                    w={'32px'}
+                    _hover={{
+                      bg: 'grayModern.50'
+                    }}
+                    onClick={() => remove(index)}
+                  >
+                    <MyIcon
+                      name={'delete'}
+                      color={'grayModern.600'}
+                      w={'16px'}
+                      h={'16px'}
+                      _hover={{
+                        color: 'red.600'
+                      }}
+                    />
+                  </Button>
+                  {index === fields.length - 1 && (
                     <AppendJSONFormItemButton
                       onClick={() =>
                         append({
@@ -176,116 +192,16 @@ export const Filter = ({
                         })
                       }
                     />
-                  ) : (
-                    <Center flex={1}>
-                      <Text
-                        fontSize={'12px'}
-                        fontWeight={'400'}
-                        lineHeight={'16px'}
-                        color={'grayModern.500'}
-                        py={'4px'}
-                      >
-                        {t('no_data_available')}
-                      </Text>
-                    </Center>
                   )}
-
-                  {fields.map((field, index) => (
-                    <Flex key={field.id} w={'fit-content'} gap={'2px'}>
-                      <MySelect
-                        height="32px"
-                        minW={'160px'}
-                        bg={'white'}
-                        color={'grayModern.600'}
-                        placeholder={t('field_name')}
-                        value={formHook.watch(`jsonFilters.${index}.key`)}
-                        list={formHook.watch('filterKeys')}
-                        onchange={(val: string) =>
-                          formHook.setValue(`jsonFilters.${index}.key`, val)
-                        }
-                      />
-                      <MySelect
-                        height="32px"
-                        minW={'60px'}
-                        bg={'white'}
-                        color={'grayModern.600'}
-                        value={formHook.watch(`jsonFilters.${index}.mode`)}
-                        list={
-                          [
-                            { value: '=', label: t('equal') },
-                            { value: '!=', label: t('not_equal') },
-                            { value: '~', label: t('contains') },
-                            { value: '!~', label: t('not_contains') }
-                          ] as { value: JsonFilterItem['mode']; label: string }[]
-                        }
-                        onchange={(val: string) =>
-                          formHook.setValue(
-                            `jsonFilters.${index}.mode`,
-                            val as JsonFilterItem['mode']
-                          )
-                        }
-                      />
-                      <Input
-                        maxW={'160px'}
-                        placeholder={t('value')}
-                        bg={'white'}
-                        value={formHook.watch(`jsonFilters.${index}.value`)}
-                        onChange={(e) =>
-                          formHook.setValue(`jsonFilters.${index}.value`, e.target.value)
-                        }
-                        border={'1px solid #E8EBF0'}
-                        boxShadow={
-                          '0px 1px 2px 0px rgba(19, 51, 107, 0.05),0px 0px 1px 0px rgba(19, 51, 107, 0.08)'
-                        }
-                      />
-                      <Button
-                        variant={'outline'}
-                        h={'32px'}
-                        w={'32px'}
-                        _hover={{
-                          bg: 'grayModern.50'
-                        }}
-                        onClick={() => remove(index)}
-                      >
-                        <MyIcon
-                          name={'delete'}
-                          color={'grayModern.600'}
-                          w={'16px'}
-                          h={'16px'}
-                          _hover={{
-                            color: 'red.600'
-                          }}
-                        />
-                      </Button>
-                      {index === fields.length - 1 && (
-                        <AppendJSONFormItemButton
-                          onClick={() =>
-                            append({
-                              key: '',
-                              value: '',
-                              mode: '='
-                            })
-                          }
-                        />
-                      )}
-                    </Flex>
-                  ))}
                 </Flex>
-              )}
+              ))}
             </Flex>
-          </FloatingBox>
-        )}
-      </Portal>
-    </Box>
+          )}
+        </Flex>
+      </PopoverContent>
+    </Popover>
   );
 };
-
-// 悬浮框组件
-const FloatingBox = React.forwardRef<HTMLDivElement, BoxProps>((props, ref) => {
-  return <Box ref={ref} {...props} />;
-});
-
-FloatingBox.displayName = 'FloatingBox';
 
 const AppendJSONFormItemButton = (props: ButtonProps) => {
   const { t } = useTranslation();
