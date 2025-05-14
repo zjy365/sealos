b3711dbd0fe105417c059c77a2688ca4197e27e2
xudaotutou <leiyikai788@gmail.com>
2025-05-14 13:32:29 +0800

    Update countdown display in PhoneCheck component
    Improve the user experience by showing a dynamic countdown when a new link can be requested. The countdown updates every second, enhancing clarity and responsiveness.
    
    onboarding for usw
    
    add cloudflare
    
    Refactor EmailCheck and update signup store
    
    Update EmailCheck.tsx to improve token handling and error recovery. Refactor signup store to manage startTime more robustly. Add brand-specific meta tags to _document.tsx.

--- frontend/providers/template/src/pages/_document.tsx
+++ frontend/providers/template/src/pages/_document.tsx
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script, { ScriptProps } from 'next/script';
 
 export default function Document() {
   const brandName = process.env.NEXT_PUBLIC_BRAND_NAME;
-
+  const scripts: ScriptProps[] = JSON.parse(process.env.CUSTOM_SCRIPTS ?? '[]');
+  // console.log('scripts', scripts);
   return (
     <Html lang="en">
       <Head>
@@ -13,6 +15,9 @@ export default function Document() {
         <meta name="apple-mobile-web-app-title" content={brandName} />
         <meta name="description" content={`${brandName} cloud`} />
         <meta name="format-detection" content="telephone=no" />
+        {scripts.map((script, i) => (
+          <Script key={i} {...script} />
+        ))}
       </Head>
       <body>
         <Main />
