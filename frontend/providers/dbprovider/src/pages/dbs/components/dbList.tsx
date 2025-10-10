4324d77917cdb44b26d5ad05e32c2729b3800416
zjy365 <3161362058@qq.com>
2025-10-10 18:26:35 +0800

    fix ci build

--- frontend/providers/dbprovider/src/pages/dbs/components/dbList.tsx
+++ frontend/providers/dbprovider/src/pages/dbs/components/dbList.tsx
@@ -335,6 +335,13 @@ const DBList = ({
     return name.includes(searchTerm) || remark.includes(searchTerm);
   };
 
+  const getDBLabel = (dbType: string) => {
+    if (dbType === 'apecloud-mysql' || dbType === 'mysql') {
+      return 'MySQL';
+    }
+    return DBTypeList.find((i) => i.id === dbType)?.label || dbType;
+  };
+
   const columns = useMemo<Array<ColumnDef<DBListItemType>>>(
     () => [
       {
@@ -452,7 +459,7 @@ const DBList = ({
               alt={row.original.id}
               src={`/images/${row.original.dbType}.svg`}
             />
-            {DBTypeList.find((i) => i.id === row.original.dbType)?.label}
+            {getDBLabel(row.original.dbType)}
           </Flex>
         )
       },
